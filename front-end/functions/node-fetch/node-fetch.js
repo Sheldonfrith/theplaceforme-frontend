59872d1442f2a7549d4cc9759da3841d14997ac9
Sheldon Frith <frith1993@yahoo.ca>
2020-09-21 15:15:33 -0400

    have to proxy http requests through netlify functions

---
+++ front-end/functions/node-fetch/node-fetch.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const fetch = require('node-fetch')
+exports.handler = async function(event, context) {
+  try {
+    const url = event.queryStringParameters.url;
+    const authorization = JSON.parse(event.queryStringParameters.authorization);
+    const method = event.httpMethod;
+    const body = event.body;
+    const headers = event.headers;
+
+    const response = await fetch(url, {
+      method: method,
+      headers: {...headers, authorization: authorization},
+      body: body,
+    })
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+    }
+  }
+}
--- front-end/src/lib/HTTP.js
+++ front-end/src/lib/HTTP.js
@@ -1,16 +1,23 @@
 import { auth } from "../components/App";
 
 const baseURL = 'http://api.theplacefor.me';
+const functionBaseURL = '/.netlify/functions/node-fetch';
 
 export const getRequest = async (endpoint) => {
   const url = baseURL+endpoint;
   const token = await auth().currentUser.getIdToken();
-  console.log('making api request to '+url+' with token '+(token));
-  const response = await fetch(url, {
-    headers: { authorization: `Bearer ${token}` },
-  }).then(data => data?data.json():null)
-  .catch(error => {console.log(error)});
-  console.log(response);
+  const authorization = `Bearer ${token}`;
+  const functionURL = functionBaseURL + '/?url='+url+'?authorization='+authorization;
+  const response = await fetch(functionURL)
+  .then(data=>data?data.json():null)
+  .catch(error=>{console.log(error)});
+
+  // console.log('making api request to '+url+' with token '+(token));
+  // const response = await fetch(url, {
+  //   headers: { authorization: `Bearer ${token}` },
+  // }).then(data => data?data.json():null)
+  // .catch(error => {console.log(error)});
+  // console.log(response);
 
   return response? response: null;
 };
@@ -18,17 +25,19 @@ export const getRequest = async (endpoint) => {
 export const postRequest = async (endpoint, body) => {
     const url = baseURL+endpoint;
     const token = await auth().currentUser.getIdToken();
-    console.log('making post request to '+url+' with token '+(token));
+    const authorization =`Bearer ${token}`;
+    const functionURL = functionBaseURL+'?url='+url+'?authorization='+authorization;
+   
+    console.log('making post request to '+url);
     const response = await fetch(url,{
         method: 'POST',
         body: JSON.stringify(body),
         headers: {
-            'Content-Type':'application/json',
-            authorization: `Bearer ${token}`
+            'Content-Type':'application/json'
         },
     }).then(data=> data?data.json():null)
     .catch(error=>{console.log(error);});
 
-    console.log(response);
+    // console.log(response);
     return response? response: null;
 }
